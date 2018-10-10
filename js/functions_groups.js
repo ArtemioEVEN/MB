f953f6c2dd4b89710d7ec686dd6e075b1c35c77c
ArtemioEVEN <43868397+ArtemioEVEN@users.noreply.github.com>
2018-10-10 00:42:56 -0500

    Update functions_groups.js

--- js/functions_groups.js
+++ js/functions_groups.js
@@ -1,1 +1,8 @@
-
+function get_events_form(){// Llama al formulario para agregar EVENTOS o MASTERS
+	jQuery('li').removeClass('active');//Para eliminar el señuelo de activo en cualquier otra opcion
+	jQuery('#events').addClass('active');//Para activar el señuelo de activo en la opcion seleccionada
+	jQuery('.row').html('<h5 class="subtitle">Cargando formulario para agregar Grupos...</h5><br />');//Mensaje de espera
+	jQuery('.pageicon').html('<span class="iconfa-book"></span>');//Cambiamos el icono en titulo
+	jQuery('.pagetitle').html('<h5>Agregar, Editar y Consultar</h5><h1>Definir Grupos</h1>');//Cambiamos mensaje en titulo
+	x_get_events_form(0, Content_general);//Llamada a PHP
+}
