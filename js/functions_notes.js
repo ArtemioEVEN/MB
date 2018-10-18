0eb349a98e69981dfdd204cafb90755e5944515b
ArtemioEVEN <43868397+ArtemioEVEN@users.noreply.github.com>
2018-10-17 19:11:50 -0500

    Update functions_notes.js

--- js/functions_notes.js
+++ js/functions_notes.js
@@ -35,3 +35,66 @@ function show_popup_notes_summary(session,type,info_ses,type_note,info_temp,read
 	jQuery("#hider_"+type).fadeIn("slow");
     jQuery('#popup_box_'+type).fadeIn("slow");
 }
+function save_update_session_note(type_note){
+	var info_session = '';
+	if (info_session = document.getElementById('info_sess') != null) { info_session = document.getElementById('info_sess').value; }
+	var note = '';
+	if (type_note == 1) { note = jQuery('#notes_sesion_temp').html(); note = replace_string(note, '"', "____"); }//TEMPLATE
+	if (type_note == 2) { note = jQuery('#notes_sesion_event').html(); note = replace_string(note, '"', "____"); }//EVENTO
+	if (type_note == 3) { note = jQuery('#notes_sesion_event_sec').html(); note = replace_string(note, '"', "____"); }//SECUNDARIO
+	if (type_note == 4) { note = jQuery('#notes_event_week').html(); note = replace_string(note, '"', "____"); }//NOTA SEMANAL
+	x_save_update_session_note(type_note,info_session,note, Content_process);//origen@-@response@-@paramtr
+}
+function close_load_extra_notes(){ jQuery('#extra_notes').hide(); }
+function save_relation_note(id_note_session, id_sec_event, id_check){
+	var func = 0;
+	if ( document.getElementById(id_check).checked ) { func = 1; }
+	x_save_relation_note(id_note_session,id_sec_event,func, Content_process);
+}
+function save_relation_note_hide(id_note_session, id_sec_event, id_lev_event, id_check){
+	var func = 0;
+	if ( document.getElementById(id_check).checked ) { func = 1; }
+	x_save_relation_note_hide(id_note_session,id_sec_event,id_lev_event,func, Content_process);
+}
+function show_popup_notes_week(type,row,read_only){
+	var id_event = document.getElementById('name_master_sel').value;
+	var id_level = document.getElementById('level_master').value;
+	x_show_session_notes_week(type,row,id_event,id_level,read_only, Content_popup_notes);
+	jQuery("#hider_"+type).fadeIn("slow");
+    jQuery('#popup_box_'+type).fadeIn("slow");
+}
+function show_popup_notes_week_summary(type,row,id_event,id_level,read_only){
+	x_show_session_notes_week(type,row,id_event,id_level,read_only, Content_popup_notes);
+	jQuery("#hider_"+type).fadeIn("slow");
+    jQuery('#popup_box_'+type).fadeIn("slow");
+}
+function delete_session_note(id_note, type_note){
+	var message_confirm = '';
+	if (type_note == 3) { // Secundario
+		message_confirm = 'La nota del Evento Secundario será eliminada y ya no podrás recuperarla, Deseas continuar?';
+	}
+	if (type_note == 2) { // Evento
+		message_confirm = 'Esta nota podría estar siendo utilizada en algunos Grupos Secundarios, si la eliminas, tambien dejarás de verla en ellos, deseas continuar?';
+	}
+	if (type_note == 1) { // Template
+		message_confirm = 'Esta nota podría estar siendo utilizada en algunos Grupos y Secundarios, si la eliminas, tambien dejarás de verla en ellos, deseas continuar?';
+	}
+	if (confirm(message_confirm)) { x_delete_session_note(id_note, Content_process); }else{ jQuery().toastmessage('showErrorToast', 'Eliminación Cancelada...'); }
+}
+function load_sessions_nf(){
+	jQuery('li').removeClass('active');
+	jQuery('#summary').addClass('active');
+	jQuery('.row').html('<h5 class="subtitle">Cargando Sesioes No Encontradas En Plataforma...</h5><br />');
+	x_load_sessions_nf(0, Content_universal);
+}
+function confirm_check_session_not_found(id_sess,user){
+	swal({
+		title: "Registro Sesión no Encontrada",
+		text: "La sesión se registrará como revisada por "+user+"\nDeseas continuar?",
+		type: "warning",
+		showCancelButton: true,
+		confirmButtonColor: "#EA2734",
+		confirmButtonText: "Si, Registrar!"
+	},
+	function(){ x_check_session_not_found(id_sess, Content_process); });
+}
